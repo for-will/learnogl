aa75385b3aa1b27f33854ceac2825d16174bc969
xuyz <937134366@qq.com>
2018-11-07 23:09:44 +0800

    triangle

--- src/hellotriangle/hellotriangle.cpp
+++ src/hellotriangle/hellotriangle.cpp
@@ -112,12 +112,15 @@ int main(void){
     glGenVertexArrays(1, &VAO);
 
     glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertexes), vertexes, GL_STATIC_DRAW);
-
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (void*)0);
     glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+
+
 
     while (!glfwWindowShouldClose(window)){
         glfwPollEvents();
@@ -133,6 +136,8 @@ int main(void){
         glfwSwapBuffers(window);
     }
 
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
     glfwTerminate();
     return 0;
 }
