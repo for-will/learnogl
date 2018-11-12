23c973044f01c23bf4da02e0760abe2d209070ce
xuyz <937134366@qq.com>
2018-11-12 22:20:51 +0800

    trans

--- src/shader/test_trans_fragment.glsl
+++ src/shader/test_trans_fragment.glsl
@@ -1,5 +1,11 @@
 #version 330 core
+in vec2 texCoord;
 out vec4 color;
+uniform sampler2D ourTexture0;
+uniform sampler2D ourTexture1;
+
 void main(){
-    color = vec4(0.5, 0.2, 0, 1.0);
+//    color = vec4(0.5, 0.2, 0, 1.0);
+    color = mix(texture( ourTexture0, texCoord), texture(ourTexture1, vec2(texCoord.x, 1-texCoord.y)), 0.2);
+//    color = vec4(texCoord, 0, 1);
 }
